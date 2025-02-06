d3491e6481a82d96c0cc37268f6e73a944de2025
Pablo Viano <pabloviano13@gmail.com>
2025-02-06 18:02:00 -0300

    test: Unfollow and Follow integration test

--- src/test/java/com/project/be_java_hisp_w29_g10/Unit/controller/UserControllerTest.java
+++ src/test/java/com/project/be_java_hisp_w29_g10/Unit/controller/UserControllerTest.java
@@ -1,17 +1,38 @@
 package com.project.be_java_hisp_w29_g10.Unit.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@AutoConfigureMockMvc
+@SpringBootTest
 class UserControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void followSeller() {
+    void followSeller() throws Exception {
+        mockMvc.perform(post("/users/{userId}/follow/{userIdToFollow}",1L,5L))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("message").value("El usuario 1 ahora sigue al vendedor 5"));
     }
 
     @Test
-    void unfollowSeller() {
+    void unfollowSeller() throws Exception{
+        mockMvc.perform(post("/users/{userId}/unfollow/{userIdToFollow}",1L,4L))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("message").value("El usuario 1 a dejado de seguir al vendedor 4"));
     }
 
     @Test
