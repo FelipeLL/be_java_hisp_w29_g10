c1bc6b71d646156cb551b93f6c5259aef0e830e1
FelipeLL <f3lip3.3z@hotmail.com>
2025-02-06 09:35:23 -0500

    feat: add enum validation exception handler

--- src/main/java/com/project/be_java_hisp_w29_g10/exception/ExceptionController.java
+++ src/main/java/com/project/be_java_hisp_w29_g10/exception/ExceptionController.java
@@ -3,8 +3,14 @@ package com.project.be_java_hisp_w29_g10.exception;
 import com.project.be_java_hisp_w29_g10.dto.ExceptionDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ExceptionController {
@@ -25,5 +31,31 @@ public class ExceptionController {
     public ResponseEntity<?> conflictException(ConflictException e) {
         ExceptionDto exceptionDto = new ExceptionDto(e.getMessage());
         return new ResponseEntity<>(exceptionDto, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<?> enumValidationException(MethodArgumentTypeMismatchException e) {
+        String validValues = "";
+
+        // Get the required type (enum class)
+        Class<?> enumType = e.getRequiredType();
+
+        // Check if it's an enum and get its values
+        if (enumType != null && enumType.isEnum()) {
+            validValues = Arrays.stream(enumType.getEnumConstants())
+                    .map(Object::toString)
+                    .collect(Collectors.joining(", "));
+        }
+
+        String errorMessage = String.format(
+                "El valor '%s' no es válido para el parámetro '%s'. Los valores permitidos son: [%s]",
+                e.getValue(),
+                e.getName(),
+                validValues
+        );
+
+        ExceptionDto exceptionDto = new ExceptionDto(errorMessage);
+
+        return new ResponseEntity<>(exceptionDto, HttpStatus.BAD_REQUEST);
     }
 }
