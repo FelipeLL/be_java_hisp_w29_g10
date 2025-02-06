8c7bb0006fd2641541ea29e3df5e4e280d54b4d4
FelipeLL <f3lip3.3z@hotmail.com>
2025-02-06 09:40:47 -0500

    test: implement ascending and descending name sort tests

--- src/test/java/com/project/be_java_hisp_w29_g10/Unit/service/SellerServiceImplTest.java
+++ src/test/java/com/project/be_java_hisp_w29_g10/Unit/service/SellerServiceImplTest.java
@@ -1,8 +1,38 @@
 package com.project.be_java_hisp_w29_g10.Unit.service;
 
+import com.project.be_java_hisp_w29_g10.dto.response.FollowerDto;
+import com.project.be_java_hisp_w29_g10.dto.response.SellerFollowersDto;
+import com.project.be_java_hisp_w29_g10.enums.NameSort;
+import com.project.be_java_hisp_w29_g10.service.SellerServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@ExtendWith(MockitoExtension.class)
 class SellerServiceImplTest {
+    SellerFollowersDto sellerFollowers;
+
+    @InjectMocks
+    SellerServiceImpl sellerService;
+
+    @BeforeEach
+    void setUp() {
+        FollowerDto follower1 = new FollowerDto(101L, "Alice");
+        FollowerDto follower2 = new FollowerDto(102L, "Bob");
+        FollowerDto follower3 = new FollowerDto(103L, "Charlie");
+
+        sellerFollowers = new SellerFollowersDto(100L, "Juan Pablo", Arrays.asList(follower2, follower3, follower1));
+    }
 
     @Test
     void getCountFollowers() {
@@ -17,6 +47,40 @@ class SellerServiceImplTest {
     }
 
     @Test
-    void orderByName() {
+    @DisplayName("US-0008: Happy path ordenamiento ascendente")
+    void orderByNameASCOkTest() {
+        // Arrange
+        NameSort order = NameSort.NAME_ASC;
+        List<String> followerNamesExpected = Arrays.asList("Alice", "Bob", "Charlie");
+
+        // Act
+        SellerFollowersDto sellerFollowersActual = sellerService.OrderByName(sellerFollowers, order);
+        List<String> followerNamesActual = extractNames(sellerFollowersActual.getFollowers());
+
+        // Assert
+        assertThat(followerNamesActual).containsExactlyElementsOf(followerNamesExpected);
+
+    }
+
+    @Test
+    @DisplayName("US-0008: Happy path ordenamiento descendente")
+    void orderByNameDESCOkTest() {
+        // Arrange
+        NameSort order = NameSort.NAME_DESC;
+        List<String> followerNamesExpected = Arrays.asList("Charlie", "Bob", "Alice");
+
+        // Act
+        SellerFollowersDto sellerFollowersActual = sellerService.OrderByName(sellerFollowers, order);
+        List<String> followerNamesActual = extractNames(sellerFollowersActual.getFollowers());
+
+        // Assert
+        assertThat(followerNamesActual).containsExactlyElementsOf(followerNamesExpected);
+    }
+
+
+    private List<String> extractNames(List<FollowerDto> followers) {
+        return followers.stream()
+                .map(FollowerDto::getUser_name)
+                .toList();
     }
 }
--- src/test/java/com/project/be_java_hisp_w29_g10/Unit/service/UserServiceImplTest.java
+++ src/test/java/com/project/be_java_hisp_w29_g10/Unit/service/UserServiceImplTest.java
@@ -1,10 +1,39 @@
 package com.project.be_java_hisp_w29_g10.Unit.service;
 
+import com.project.be_java_hisp_w29_g10.dto.response.FollowedSellerDto;
+import com.project.be_java_hisp_w29_g10.dto.response.FollowerDto;
+import com.project.be_java_hisp_w29_g10.dto.response.SellerFollowersDto;
+import com.project.be_java_hisp_w29_g10.dto.response.UserFollowedSellerDto;
+import com.project.be_java_hisp_w29_g10.enums.NameSort;
+import com.project.be_java_hisp_w29_g10.service.UserServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
+    UserFollowedSellerDto userFollowedSellers;
+
+    @InjectMocks
+    UserServiceImpl userService;
+
+    @BeforeEach
+    void setUp() {
+        FollowedSellerDto follower1 = new FollowedSellerDto(101L, "Alice");
+        FollowedSellerDto follower2 = new FollowedSellerDto(102L, "Bob");
+        FollowedSellerDto follower3 = new FollowedSellerDto(103L, "Charlie");
+
+        userFollowedSellers = new UserFollowedSellerDto(100L, "Juan Pablo", Arrays.asList(follower2, follower3, follower1));
+    }
 
     @Test
     void followSeller() {
@@ -19,6 +48,40 @@ class UserServiceImplTest {
     }
 
     @Test
-    void orderByName() {
+    @DisplayName("US-0008: Happy path ordenamiento ascendente")
+    void orderByNameASCOkTest() {
+        // Arrange
+        NameSort order = NameSort.NAME_ASC;
+        List<String> followedSellerNamesExpected = Arrays.asList("Alice", "Bob", "Charlie");
+
+        // Act
+        UserFollowedSellerDto userFollowedSellersActual = userService.OrderByName(userFollowedSellers, order);
+        List<String> followerNamesActual = extractNames(userFollowedSellersActual.getFollowed());
+
+        // Assert
+        assertThat(followerNamesActual).containsExactlyElementsOf(followedSellerNamesExpected);
+
+    }
+
+    @Test
+    @DisplayName("US-0008: Happy path ordenamiento descendente")
+    void orderByNameDESCOkTest() {
+        // Arrange
+        NameSort order = NameSort.NAME_DESC;
+        List<String> followedSellerNamesExpected = Arrays.asList("Charlie", "Bob", "Alice");
+
+        // Act
+        UserFollowedSellerDto userFollowedSellersActual = userService.OrderByName(userFollowedSellers, order);
+        List<String> followerNamesActual = extractNames(userFollowedSellersActual.getFollowed());
+
+        // Assert
+        assertThat(followerNamesActual).containsExactlyElementsOf(followedSellerNamesExpected);
+
+    }
+
+    private List<String> extractNames(List<FollowedSellerDto> followers) {
+        return followers.stream()
+                .map(FollowedSellerDto::getUser_name)
+                .toList();
     }
 }
